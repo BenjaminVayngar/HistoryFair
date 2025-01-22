c53fdc48bbb3f8aee8365b6e476989536f64c008
BenjaminVayngar <benjamin.vayngart@vhhscougars.org>
2025-01-21 18:19:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-  document.getElementById("mySidenav").style.width = "350px";
+  document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   //document.body.style.backgroundImage = "rgba(0,0,0,0.4)";
 }
