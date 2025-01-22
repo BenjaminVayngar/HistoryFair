50606f6f3e0b43d97b8e3d8e2a0ea2d46ec372ec
BenjaminVayngar <benjamin.vayngart@vhhscougars.org>
2025-01-21 18:25:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("mySidenav").style.width = "300px";
+  document.getElementById("mySidenav").style.height = "300px";
   document.getElementById("main").style.marginLeft = "250px";
   //document.body.style.backgroundImage = "rgba(0,0,0,0.4)";
 }
@@ -9,6 +9,6 @@ function openNav() {
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.height = "0";
   document.getElementById("main").style.marginLeft = "0";
 }
